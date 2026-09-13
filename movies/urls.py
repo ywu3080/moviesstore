b6d899ba3015c989629e7578269030a082a6ffca
ywu3080 <ywu3080@gatech.edu>
2026-09-12 21:47:33 -0400

    report button v2

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
     path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
     path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
-    path('<int:id>/review/<int:review_id>/delete/', views.report_review, name='movies.report_review'),
+    path('<int:id>/review/<int:review_id>/report/', views.report_review, name='movies.report_review'),
 ]
