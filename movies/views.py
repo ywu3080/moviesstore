f5199381ea8a906517028a84fbe03c29868e43d9
ywu3080 <ywu3080@gatech.edu>
2026-09-12 21:41:22 -0400

    added the report button

--- movies/views.py
+++ movies/views.py
@@ -60,3 +60,9 @@ def delete_review(request, id, review_id):
         user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
+
+@login_required
+def report_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    review.delete()
+    return redirect('movies.show', id=id)
